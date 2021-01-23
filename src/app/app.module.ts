162392b1362e3e3a56fb1e896f62b6dc85b154d0
Leith Osborne <osborneleith@gmail.com>
2021-01-23 11:39:12 +0000

    added test component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ImageSlicerComponent } from './components/image-slicer/image-slicer.component';
+import { TestComponent } from './components/test/test.component';
 
 @NgModule({
-  declarations: [AppComponent, ImageSlicerComponent],
+  declarations: [AppComponent, ImageSlicerComponent, TestComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
