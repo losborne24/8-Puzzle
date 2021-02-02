598cd5a73b250b6530a8de9d59bdfa3435d58942
Leith Osborne <osborneleith@gmail.com>
2021-02-02 21:08:09 +0000

    drag and drop upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ImageSlicerComponent } from './components/image-slicer/image-slicer.component';
 import { TestComponent } from './components/test/test.component';
+import { DragAndDropComponent } from './components/drag-and-drop/drag-and-drop.component';
+import { DragAndDropDirective } from './directives/drag-and-drop.directive';
+
+import { ProgressComponent } from './components/progress/progress.component';
 
 @NgModule({
-  declarations: [AppComponent, ImageSlicerComponent, TestComponent],
+  declarations: [
+    AppComponent,
+    ImageSlicerComponent,
+    TestComponent,
+    DragAndDropDirective,
+    ProgressComponent,
+    DragAndDropComponent,
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/drag-and-drop/drag-and-drop.component.ts
+++ src/app/components/drag-and-drop/drag-and-drop.component.ts
@@ -1,12 +1,11 @@
-import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-test',
-  templateUrl: './test.component.html',
-  styleUrls: ['./test.component.scss'],
+  selector: 'app-drag-and-drop',
+  templateUrl: './drag-and-drop.component.html',
+  styleUrls: ['./drag-and-drop.component.scss'],
 })
-export class TestComponent implements OnInit {
-  @ViewChild('fileDropRef', { static: false }) fileDropEl: ElementRef;
+export class DragAndDropComponent implements OnInit {
   files: any[] = [];
 
   /**
@@ -28,10 +27,6 @@ export class TestComponent implements OnInit {
    * @param index (File index)
    */
   deleteFile(index: number) {
-    if (this.files[index].progress < 100) {
-      console.log('Upload in progress.');
-      return;
-    }
     this.files.splice(index, 1);
   }
 
@@ -64,7 +59,6 @@ export class TestComponent implements OnInit {
       item.progress = 0;
       this.files.push(item);
     }
-    this.fileDropEl.nativeElement.value = '';
     this.uploadFilesSimulator(0);
   }
 
@@ -73,12 +67,12 @@ export class TestComponent implements OnInit {
    * @param bytes (File size in bytes)
    * @param decimals (Decimals point)
    */
-  formatBytes(bytes, decimals = 2) {
+  formatBytes(bytes, decimals) {
     if (bytes === 0) {
       return '0 Bytes';
     }
     const k = 1024;
-    const dm = decimals <= 0 ? 0 : decimals;
+    const dm = decimals <= 0 ? 0 : decimals || 2;
     const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
     const i = Math.floor(Math.log(bytes) / Math.log(k));
     return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
