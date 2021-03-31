788f5065af6549695714a2795f6f86911ab23ad9
Leith Osborne <osborneleith@gmail.com>
2021-03-31 16:17:13 +0100

    renamed to 8-puzzle

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/image-slicer'),
+      dir: require('path').join(__dirname, './coverage/8-puzzle'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,9 +4,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -16,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'image-slicer'`, () => {
+  it(`should have as title '8-puzzle'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('image-slicer');
+    expect(app.title).toEqual('8-puzzle');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('image-slicer app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain(
+      '8-puzzle app is running!'
+    );
   });
 });
