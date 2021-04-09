ec5e2b7a471b781bd0dc04975cf87a5777d571ee
Leith Osborne <osborneleith@gmail.com>
2021-04-09 14:10:58 +0100

    bring ot front on drag

--- src/app/directives/drag-and-drop-demo-images.directive.ts
+++ src/app/directives/drag-and-drop-demo-images.directive.ts
@@ -43,13 +43,17 @@ export class DragAndDropDemoImagesDirective {
           // keep the dragged position in the data-x/data-y attributes
           var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx;
           var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
-
+          target.style.zIndex = 10;
           // translate the element
           target.style.webkitTransform = target.style.transform =
             'translate(' + x + 'px, ' + y + 'px)';
           // update the posiion attributes
           target.setAttribute('data-x', x);
           target.setAttribute('data-y', y);
+        },
+        end(event) {
+          var target = event.target;
+          target.style.zIndex = 1;
         },
       },
       modifiers: [this.restrictionBox],
