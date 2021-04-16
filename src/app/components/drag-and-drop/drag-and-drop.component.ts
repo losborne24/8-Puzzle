b2ff591533e6aa62a965c8f45a5ecbc554c7d99f
Leith Osborne <osborneleith@gmail.com>
2021-04-16 10:14:19 +0100

    feat: show image only after load

--- src/app/components/drag-and-drop/drag-and-drop.component.ts
+++ src/app/components/drag-and-drop/drag-and-drop.component.ts
@@ -42,4 +42,7 @@ export class DragAndDropComponent implements OnInit {
   onFileUpload(file) {
     this.imageFile.emit(file);
   }
+  onImageLoad($event) {
+    $event.target.hidden = false;
+  }
 }
